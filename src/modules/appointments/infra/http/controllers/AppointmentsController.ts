14f14f0ab23247fe85a4c1684131b0ae52eab239
Beatriz Murase <biamurase@gmail.com>
2020-05-22 18:56:04 -0300

    fix: include user data in appointments index

--- src/modules/appointments/infra/http/controllers/AppointmentsController.ts
+++ src/modules/appointments/infra/http/controllers/AppointmentsController.ts
@@ -6,8 +6,8 @@ import CreateAppointmentService from '@modules/appointments/services/CreateAppoi
 
 class AppointmentsController {
   public async create(request: Request, response: Response): Promise<Response> {
-    // const user_id = request.user.id;
-    const { user_id, provider_id, date } = request.body;
+    const user_id = request.user.id;
+    const { provider_id, date } = request.body;
 
     // const parsedDate = parseISO(date);
 
