fd92ace16b3d899f28c5588746dd90006b31a7ed
Beatriz Murase <biamurase@gmail.com>
2020-05-22 23:18:13 -0300

    test: recover appointments list from cache

--- src/modules/appointments/services/ListProviderAppointmentsService.spec.ts
+++ src/modules/appointments/services/ListProviderAppointmentsService.spec.ts
@@ -39,4 +39,38 @@ describe('ListProviderAppointments', () => {
 
     expect(appointments).toEqual([appoint1, appoint2]);
   });
+
+  it('should be able to recover appointments list from cache', async () => {
+    await fakeAppointmentsRepository.create({
+      provider_id: 'provider',
+      user_id: 'user1',
+      date: new Date(2020, 4, 20, 14, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'provider',
+      user_id: 'user2',
+      date: new Date(2020, 4, 20, 15, 0, 0),
+    });
+
+    let appointments = await listProviderAppointments.execute({
+      provider_id: 'provider',
+      day: 20,
+      month: 5,
+      year: 2020,
+    });
+
+    const recoveredData = await fakeCacheProvider.recover(
+      `provider-appointments:provider:2020-5-20`,
+    );
+
+    appointments = await listProviderAppointments.execute({
+      provider_id: 'provider',
+      day: 20,
+      month: 5,
+      year: 2020,
+    });
+
+    expect(appointments).toEqual(recoveredData);
+  });
 });
